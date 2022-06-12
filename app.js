7839d2e4f36776d057f9d1ec634a1778abad8ddf
Robin Roy <robin.rrt123gmail.com>
2022-06-13 00:31:26 +0530

    landing page and login page with validation frontend

--- app.js
+++ app.js
@@ -3,10 +3,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var firebase = require('firebase-admin');
+require('firebase/auth');
+  require('firebase/database');
+  // Initialize Firebase for the application
+  var config = {
+      apiKey: process.env.apiKey,
+      authDomain: process.env.authDomain,
+      databaseURL: process.env.databaseURL,
+      storageBucket: process.env.storageBucket,
+      messagingSenderId: process.env.messagingSenderId
+    };
+
+    
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var loginRouter = require('./routes/login');
+var registerRouter = require('./routes/register');
 var app = express();
 
 // view engine setup
@@ -21,6 +35,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+app.use('/register', registerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
